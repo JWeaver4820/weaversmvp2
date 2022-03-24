99fb8eb60070cb1d7c0f7918d970de18cadd3f3b
JWeaver4820 <jweaver@unca.edu>
2022-03-24 19:37:23 -0400

    Add files via upload

--- lib/pages/auth/subpages/Metabolism/metabolism.dart
+++ lib/pages/auth/subpages/Metabolism/metabolism.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:rxdart/rxdart.dart';
 import 'package:weaversmvp/models/user.dart';
+import 'package:weaversmvp/pages/auth/subpages/Metabolism/weight_chart.dart';
 import 'package:weaversmvp/pages/auth/subpages/Metabolism/weight_list.dart';
 import 'package:weaversmvp/pages/auth/subpages/profile_screen.dart';
 import 'package:weaversmvp/pages/homepage/home_screen_viewmodel.dart';
 
 import '../../../../operations/database.dart';
 import '../../../../weight_scheduler/weight_screen.dart';
+import 'package:charts_flutter/flutter.dart' as chart;
 
 /*class MetabolismScreen extends StatefulWidget{
   @override
@@ -24,7 +26,9 @@ class MetabolismScreen extends StatelessWidget{
 
   BehaviorSubject<int> _pages = BehaviorSubject();
 
+
   int page = -1;
+
 
   @override
   Widget build(BuildContext context) {
@@ -32,34 +36,30 @@ class MetabolismScreen extends StatelessWidget{
    return FutureBuilder<List<Weight>>(builder: (_, snapshot){
 
 
-     print("snapshotE => ${snapshot.error}");
-
-    print("snapshot => ${snapshot.data}");
-
-     List<Widget> subPages = [WeightListScreen(weighList : snapshot.data,)];
+     List<Widget> subPages = [WeightListScreen(weighList : snapshot.data,), WeightChart.startWithData(weightList: snapshot.data)];
 
      return StreamBuilder<int>(builder: (_, snapshot){
 
        return WillPopScope(child: page < 0 ? Column(
          children: [
            _buildButton("Weekly Recorded Weight", () {
-
-             _pages.sink.add(page++);
+             page = 0;
+             _pages.sink.add(page);
            }),
            _buildButton("Weight Chart", () {
-
-             _pages.sink.add(page++);
+             page = 1;
+             _pages.sink.add(page);
            }),
            _buildButton("Current State of Metabolism and Suggestions", () {
 
-             _pages.sink.add(page++);
+             //_pages.sink.add(page++);
            })
 
          ],
        ): subPages[page], onWillPop: () async {
 
-         if(page >= 0){
-           _pages.sink.add(page--);
+         if(snapshot.requireData >= 0){
+           _pages.sink.add(-1);
            return false;
          }else{
 
@@ -71,7 +71,10 @@ class MetabolismScreen extends StatelessWidget{
   }
 
   Widget _buildButton(String title, Function()? onTap){
-    return ElevatedButton(onPressed: (){onTap?.call();},
+    return ElevatedButton(onPressed: (){
+      print("hellow => 33333");
+      onTap?.call();
+      },
      child: Text(title, style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),), style: ButtonStyle(
        backgroundColor: MaterialStateProperty.all(Colors.teal)
      ),);
