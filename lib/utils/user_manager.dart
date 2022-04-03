2968b33d7a1e19d4bf2aea5b2e1b79cfbb6f5d4d
JWeaver4820 <jweaver@unca.edu>
2022-04-03 16:47:56 -0400

    Add files via upload

--- lib/utils/user_manager.dart
+++ lib/utils/user_manager.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:weaversmvp/models/user.dart';
 
-class UserManager {
+class UserManager{
 
 
     static void saveUser(User user)async{
